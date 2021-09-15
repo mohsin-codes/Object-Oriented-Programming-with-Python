f7bcad5750b3f267ab283bf72653ac5057eaca9e
Mohsin Khan <mohsink9899@gmail.com>
2021-09-16 03:34:26 +0530

    Rollback changes in Classes and Objects. Added init method file

--- Classes and Objects.py
+++ Classes and Objects.py
@@ -1,5 +1,4 @@
 class Computer:
-
     def config(self):
         print("i5, 16GB, 1TB")
 
---
+++ init method.py
@@ -0,0 +1,13 @@
+class Computer:
+    def __init__(self, cpu, ram):
+        self.cpu = cpu
+        self.ram = ram
+
+    def config(self):
+        print(self.cpu, self.ram)
+
+com1 = Computer("i5", "16GB")
+com2 = Computer("Ryzen 5", "8GB")
+
+com1.config()
+com2.config()
